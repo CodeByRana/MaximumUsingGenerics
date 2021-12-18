1dca90f315a6745c0632d087fb126c3256a092a6
deepakphpweb <dipakrana2760@gmail.com>
2021-12-18 07:50:31 +0530

    Added: compare to float maximum number integer

--- src/main/java/com/bridgelabz/generics/Maximum.java
+++ src/main/java/com/bridgelabz/generics/Maximum.java
@@ -16,4 +16,14 @@ public class Maximum{
             return c;
         }
     }
+    // Passed the Float Value
+    public static Float getMax(Float a, Float b, Float c) {
+        // Compare the Float Value
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            return a;
+        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
+            return b;
+        }
+        return c;
+    }
 }
--- src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -13,4 +13,40 @@ public class MaximumTest {
         int expectedResult = 16;
         Assert.assertEquals(expectedResult,actualResult);
     }
+    //  Test Case for the Integer where max_Value at Second Position
+    @Test
+    public void given3IntegerValue_Case2_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+        int actualResult = Maximum . getMax(10,15,11);
+        int expectedResult = 15;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    //  Test Case for the Integer where max_Value at Third Position
+    @Test
+    public void given3IntegerValue_Case3_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+        int actualResult = Maximum . getMax(11,10,12);
+        int expectedResult = 12;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    //Test Case for the Float where max_Value at First Position
+    @Test
+    public void given3FloatValue_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+        Float actualResult = Maximum . getMax(15.6f,10.5f,11.5f);
+        Float expectedResult = 15.6f;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    //  Test Case for the Float where max_Value at Second Position
+    @Test
+    public void given3FloatValue_Case2_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+        Float actualResult = Maximum . getMax(11.1f,12.5f,10.6f);
+        Float expectedResult = 12.5f;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    //  Test Case for the Float where max_Value at Third Position
+    @Test
+    public void given3FloatValue_Case3_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+        Float actualResult = Maximum . getMax(10.5f,15.6f,16.5f);
+        Float expectedResult = 16.5f;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+
 }
